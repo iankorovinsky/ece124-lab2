656f25320ef4e82d5f8b1c72e43c9c2783a424ff
Ian Korovinsky <ian.korovinsky@gmail.com>
2024-02-07 18:00:54 -0500

    Hex mux commenting

--- hex_mux.vhd
+++ hex_mux.vhd
@@ -1,19 +1,24 @@
+-- Members: Ian Korovinsky and Steven Yang
+-- Import packages
 library ieee;
 use ieee.std_logic_1164.all;
 
+-- Define hex mux entity
 entity hex_mux is
 port (
-	hex_num3, hex_num2, hex_num1, hex_num0 : in std_logic_vector(3 downto 0);
-	mux_select                             : in std_logic_vector(1 downto 0);
-	hex_out                                : out std_logic_vector(3 downto 0)
+	hex_num3, hex_num2, hex_num1, hex_num0 : in std_logic_vector(3 downto 0); -- Input 4-bit hexs
+	mux_select                             : in std_logic_vector(1 downto 0); -- Output 2-bit selector
+	hex_out                                : out std_logic_vector(3 downto 0) -- Output 4-bit hex 
 );
 end hex_mux;
 
+
+-- Define logic for hex mux
 architecture mux_logic of hex_mux is
 
 begin
 
-	-- for the multiplexing of four hex input busses
+	-- Multiplexer logic based on the selector
 	with mux_select(1 downto 0) select
 	hex_out <= hex_num0 when "00",
 				  hex_num1 when "01",
