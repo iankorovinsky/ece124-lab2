48ab6d16944a83ea31c3cc62dec592718c33b89a
Ian Korovinsky <ian.korovinsky@gmail.com>
2024-02-07 18:09:21 -0500

    Added some more commenting on last two VHDL files

--- PB_Inverters.vhd
+++ PB_Inverters.vhd
@@ -1,18 +1,24 @@
+-- Members: Ian Korovinsky and Steven Yang
+-- Import packages
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
+-- Define PB Inverters entity
 ENTITY PB_Inverters IS
 	PORT
 	(
-		pb_n : IN std_logic_vector(3 downto 0); -- input
-		pb : OUT std_logic_vector(3 downto 0) -- output
+		pb_n : IN std_logic_vector(3 downto 0); -- Input 4 bit button vector
+		pb : OUT std_logic_vector(3 downto 0) -- Output 4 bit vector
 	);
 END PB_Inverters;
 
+
+-- Define logic of PB inverters
 ARCHITECTURE gates of PB_Inverters IS
 
 BEGIN
--- The output vector 'pb' is assigned the bitwise not of input vector 'pb_n'.
+
+-- The output vector 'pb' is assigned the bitwise not of input vector 'pb_n', (0 becomes 1, 1 becomes 0)
 pb <= not(pb_n);
 
 END gates;
--- SevenSegment.vhd
+++ SevenSegment.vhd
@@ -1,7 +1,5 @@
---
---
---
---
+-- Members: Ian Korovinsky and Steven Yang
+-- Import packages
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
